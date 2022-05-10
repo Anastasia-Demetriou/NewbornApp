51e506ecdbad19b321460686f4f20c5884fe8b16
Anastasia Demetriou <anastasia.demetriou@bcgdv.com>
2022-05-10 19:43:13 +0100

    displaying ldob, list and list is filtering based on the timeline

---
+++ src/BabyChecklist.tsx
@@ -0,0 +1,7 @@
+const BabyChecklist = [
+  //object has 2 attributes name and day
+  { name: ' heel prick blood test ', day: 4 },
+  { name: 'full physical examination', day: 9 },
+  { name: 'New baby review', day: 14 },
+]
+export default BabyChecklist
--- src/DatePickerApp.tsx
+++ src/DatePickerApp.tsx
@@ -1,10 +1,23 @@
-import React, { useState } from 'react'
-import { Button } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Button, Text, View } from 'react-native'
+
 import DatePicker from 'react-native-date-picker'
+import moment from 'moment'
+
+import BabyChecklist from './BabyChecklist'
 
 export default function DatePickerApp() {
   const [date, setDate] = useState(new Date())
   const [open, setOpen] = useState(false)
+
+  const onChange = (event, seletedDate) => {
+    setDate(moment(selectedDate))
+  }
+
+  const calculateAge = (date) => {
+    const currentDate = moment().diff(date, 'days')
+    return currentDate
+  }
 
   return (
     <>
@@ -13,16 +26,37 @@ export default function DatePickerApp() {
         modal
         open={open}
         date={date}
+        value={date}
         mode='date'
         locale='en_GB'
+        minimumDate={new Date(moment().subtract(2, 'years').format())}
+        maximumDate={new Date()}
+        onChange={onChange}
         onConfirm={(date) => {
           setOpen(false)
           setDate(date)
+          calculateAge(date)
         }}
         onCancel={() => {
           setOpen(false)
         }}
       />
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginLeft: '3%',
+        }}
+      >
+        <Text>Your Baby's date of birth is: {date.toDateString()}</Text>
+        <Text> Your baby is {calculateAge(date)} days old </Text>
+        <B1> TimeLline </B1>
+        {BabyChecklist.filter((item) => item.day < 14).map((filteredItem) => {
+          return <Text>{filteredItem.name}</Text>
+        })}
+      </View>
     </>
   )
 }
+
+//age = todays date subtracted by date of birth
